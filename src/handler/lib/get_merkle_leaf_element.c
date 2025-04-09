e636ec9a3dc8b25bf43b373c57abc18247c0a10e
Chester <chester@pixelauth.com>
2025-04-09 14:18:42 +0800

    last file in list

--- src/handler/lib/get_merkle_leaf_element.c
+++ src/handler/lib/get_merkle_leaf_element.c
@@ -17,7 +17,7 @@ int call_get_merkle_leaf_element(dispatcher_context_t *dispatcher_context,
                                         merkle_root,
                                         tree_size,
                                         leaf_index,
-                                        leaf_hash);                                   
+                                        leaf_hash);
     if (res < 0) {
         return res;
     }
